8872c2a27070a0233e531444e09eb6af79a4e455
Rico Sharp <rico.sharp@gmail.com>
2019-11-02 16:14:31 -0400

    changed text formatting and moved code to function

--- aljazeera.py
+++ aljazeera.py
@@ -14,22 +14,25 @@ channel_id = "tv.aljazeera.live"
 channel_name = "Al Jazeera"
 
 epg = '<tv>\n'
-epg += '<channel id="' + channel_id + '">\n'
-epg += '\t<display-name>' + channel_name + '</display-name>\n'
+epg += '<channel id="%s">\n' % channel_id
+epg += '\t<display-name>%s</display-name>\n' % channel_name
 epg += '</channel>\n'
+
+def format_time(current_program):
+    datetime_start = datetime.datetime.strptime(response['Schedule']['Programs'][i]['TVGuideDateTime'] , '%d-%b-%Y %H:%M:%S')
+    datetime_stop = datetime.datetime.strptime(response['Schedule']['Programs'][i + 1]['TVGuideDateTime'] , '%d-%b-%Y %H:%M:%S')
+    prog_start = datetime_start.strftime('%Y%m%d%H%M%S')
+    prog_end = datetime_stop.strftime('%Y%m%d%H%M%S')
+    return prog_start, prog_end
+
 for i in range(number_of_programs):
     program_title = response['Schedule']['Programs'][i]['SeriesTitle'].replace('&' , '&amp;')
     program_desc = response['Schedule']['Programs'][i]['Synopsis'].replace('&' , '&amp;')
-    tvg_datetime_start = datetime.datetime.strptime(response['Schedule']['Programs'][i]['TVGuideDateTime'] , '%d-%b-%Y %H:%M:%S')
-    tvg_datetime_stop = datetime.datetime.strptime(response['Schedule']['Programs'][i + 1]['TVGuideDateTime'] , '%d-%b-%Y %H:%M:%S')
-    program_start = tvg_datetime_start.strftime('%Y%m%d%H%M%S') 
-    program_stop = tvg_datetime_stop.strftime('%Y%m%d%H%M%S')
-    epg += tvg_datetime_start.strftime('%Y\n')
-    epg += '<programme start="' + program_start + ' +0300' '" stop="' + program_stop + ' +0300' '" channel="' + channel_id + '">\n'
-    epg += '\t<title>' + program_title + '</title>\n'
-    epg += '\t<desc>' + program_desc + '</desc>\n'
+    program_start, program_stop = format_time(i)
+    epg += '<programme start="%s +0300" stop="%s +0300" channel="%s">\n' % (program_start, program_stop, channel_id)
+    epg += '\t<title>%s</title>\n' % program_title
+    epg += '\t<desc>%s</desc>\n' % program_desc
     epg += '</programme>\n'
 epg += '</tv>'
-
 xmltv_file = open(channel_id + '.xml' , 'w+')
 xmltv_file.write(epg)
